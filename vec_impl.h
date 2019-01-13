769260051caa9e63d5bafb738188c577474b6950
Josh <jlazarus1@gmail.com>
2019-01-13 13:57:50 +0200

    first upload

---
+++ vec_impl.h
@@ -0,0 +1,28 @@
+//
+// Created by josh on 1/13/19.
+//
+
+#ifndef HW5_VEC_IMPL_H
+#define HW5_VEC_IMPL_H
+
+#endif //HW5_VEC_IMPL_H
+
+using namespace std;
+
+
+template <class T>
+
+void Vec<T>::push_back(T el) {
+
+}
+
+template <class T>
+unsigned int Vec<T>::size() const {
+    return vals_.size;
+}
+
+template <class T>
+Vec<T> Vec<T>::operator+(const Vec &rhs) const {
+
+    if (
+}
